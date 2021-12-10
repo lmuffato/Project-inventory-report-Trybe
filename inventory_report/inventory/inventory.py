04cc8dcfbb274021e4f8924506a88d7f8a298253
iago <iago.pferreiravr@gmail.com>
2021-12-10 17:20:35 -0300

    feat: Req4
    Refatora o método import_data na classe Inventory, de forma a ler arquivos csv e json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -7,11 +8,17 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(cls, file_path, report_type="simples"):
-        with open(file_path) as file:
-            file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
-            enterprises_list = []
-            for enterprise in file_reader:
-                enterprises_list.append(enterprise)
+        enterprises_list = []
+        if file_path.endswith(".csv"):
+            with open(file_path) as file:
+                file_reader = csv.DictReader(
+                    file, delimiter=",", quotechar='"'
+                )
+                for enterprise in file_reader:
+                    enterprises_list.append(enterprise)
+        elif file_path.endswith(".json"):
+            with open(file_path) as file:
+                enterprises_list = json.load(file)
         if report_type == "simples":
             return SimpleReport.generate(enterprises_list)
         return CompleteReport.generate(enterprises_list)
