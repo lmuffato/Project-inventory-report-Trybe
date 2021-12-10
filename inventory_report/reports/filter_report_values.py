47f452b7457e938011c671c0c6a239109887bc33
Rodrigo Rech <rodrigorech92@gmail.com>
2021-12-10 17:44:31 -0300

    Feat: Simple_report and filter_report_values implemented

---
+++ inventory_report/reports/filter_report_values.py
@@ -0,0 +1,34 @@
+import statistics
+from datetime import date
+
+
+# https://medium.com/code-85/use-sets-to-count-occurrences-in-python-efec802aa3ad
+# https://docs.python.org/pt-br/3.7/howto/sorting.html
+
+class FilterValues:
+
+    def get_data_fabricacao(product_list):
+        data_fabricacao = ''
+        for product in product_list:
+            if data_fabricacao == '':
+                data_fabricacao = product['data_de_fabricacao']
+            elif product['data_de_fabricacao'] < data_fabricacao:
+                data_fabricacao = product['data_de_fabricacao']
+        return data_fabricacao
+
+    def get_data_validade(product_list):
+        data_validade = ''
+        for product in product_list:
+            if data_validade == '':
+                data_validade = product['data_de_validade']
+            elif (data_validade > product['data_de_validade']
+                  > date.today().strftime("%Y,%m, %d")):
+                data_validade = product['data_de_validade']
+        return data_validade
+
+    def get_empresa(product_list):
+        empresas = []
+        for product in product_list:
+            empresas.append(product['nome_da_empresa'])
+        empresa = statistics.mode(empresas)
+        return empresa
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,13 @@
-class simple_report:
-    pass
+from inventory_report.reports.filter_report_values import FilterValues
+
+
+class SimpleReport:
+
+    def generate(product_list):
+        data_fabricacao = FilterValues.get_data_fabricacao(product_list)
+        data_validade = FilterValues.get_data_validade(product_list)
+        empresa = FilterValues.get_empresa(product_list)
+        return (
+            f"Data de fabricação mais antiga: {data_fabricacao}\n"
+            f"Data de validade mais próxima: {data_validade}\n"
+            f"Empresa com maior quantidade de produtos estocados: {empresa}\n")
