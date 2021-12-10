bad4b29095ec2a5878e43fcff7f68e79ce66f754
iago <iago.pferreiravr@gmail.com>
2021-12-10 18:12:15 -0300

    refactor: inventory.py
    Refatorando a classe Inventory para ter métodos específicos para importar tipos específicos de arquivos

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,19 +6,30 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+    def import_csv(file_path):
+        file_data = []
+        with open(file_path) as file:
+            file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            for enterprise in file_reader:
+                file_data.append(enterprise)
+        return file_data
+
+    def import_json(file_path):
+        file_data = []
+        with open(file_path) as file:
+            file_data = json.load(file)
+        return file_data
+
     @classmethod
     def import_data(cls, file_path, report_type="simples"):
         enterprises_list = []
         if file_path.endswith(".csv"):
-            with open(file_path) as file:
-                file_reader = csv.DictReader(
-                    file, delimiter=",", quotechar='"'
-                )
-                for enterprise in file_reader:
-                    enterprises_list.append(enterprise)
+            enterprises_list = cls.import_csv(file_path)
         elif file_path.endswith(".json"):
-            with open(file_path) as file:
-                enterprises_list = json.load(file)
+            enterprises_list = cls.import_json(file_path)
         if report_type == "simples":
             return SimpleReport.generate(enterprises_list)
         return CompleteReport.generate(enterprises_list)
+
+
+Inventory.import_xml("inventory_report/data/inventory.xml")
