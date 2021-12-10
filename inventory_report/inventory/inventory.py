890b93bd106797c270df3b7124abb1aee17c95c2
iago <iago.pferreiravr@gmail.com>
2021-12-10 18:15:24 -0300

    feat: Req5
    Impementando o método import_xml na classe Inventory, essa classe é responsável por converter xml para dict

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,8 @@
 import csv
 import json
+import xmltodict
+# https://linuxhint.com/python_xml_to_dictionary/
+
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -20,6 +23,16 @@ class Inventory:
             file_data = json.load(file)
         return file_data
 
+    def import_xml(file_path):
+        file_data = []
+        with open(file_path) as file:
+            # https://omz-software.com/pythonista/docs/ios/xmltodict.html
+            for enterprise in xmltodict.parse(file.read())["dataset"][
+                "record"
+            ]:
+                file_data.append(enterprise)
+        return file_data
+
     @classmethod
     def import_data(cls, file_path, report_type="simples"):
         enterprises_list = []
@@ -27,6 +40,8 @@ class Inventory:
             enterprises_list = cls.import_csv(file_path)
         elif file_path.endswith(".json"):
             enterprises_list = cls.import_json(file_path)
+        elif file_path.endswith(".xml"):
+            enterprises_list = cls.import_xml(file_path)
         if report_type == "simples":
             return SimpleReport.generate(enterprises_list)
         return CompleteReport.generate(enterprises_list)
