731cfef0456335a82c613b2de93158f5a7268d9c
Robertson <robertson.nunes@gmail.com>
2021-12-10 20:27:20 -0300

    req3 :construction: feat csv import 3.1

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,19 @@
+import os
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    def import_data(path, type_of_report):
+        _arquivo, extensao = os.path.splitext(path)
+        report_types = {
+            "simples": SimpleReport.generate,
+            "completo": CompleteReport.generate,
+        }
+        file_extension = {
+            ".csv": CsvImporter.import_data,
+        }
+        data = file_extension[extensao](path)
+        result = report_types[type_of_report](data)
+        return result
