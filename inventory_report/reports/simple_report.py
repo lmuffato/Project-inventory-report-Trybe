9eadaf85faf2fb1f163fae64304c6e0195907c4d
rcalvs <rafaelcalvette@gmail.com>
2021-12-12 14:33:06 -0300

    Req01

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from collections import Counter
+from inventory_report.utils.functions import Functions
+
+
+class SimpleReport:
+
+  def generate(path):
+      today = datetime.today().strftime('%Y-%m-%d')
+      older = "3000-01-01"
+      validate = "3000-01-01"
+      nome = ''
+      nomes = []
+      older = Functions.data_de_fabricacao(today, path, older)
+      validate = Functions.data_de_validade(today, path, validate)
+      item = iter(path)
+      while item:
+        try:
+            result = next(item)
+            nomes.append(result['nome_da_empresa'])
+        except StopIteration:
+            break
+      nomes = Counter(nomes)
+      nome = nomes.most_common()
+      nome = nome[0][0]
+      return(
+          f"Data de fabricação mais antiga: {older}\n"
+          f"Data de validade mais próxima: {validate}\n"
+          f"Empresa com maior quantidade de produtos estocados: {nome}\n"
+      )
---
+++ inventory_report/utils/functions.py
@@ -0,0 +1,24 @@
+class Functions:
+
+    def data_de_fabricacao(today, path, older):
+        item = iter(path)
+        while item:
+            try:
+                result = next(item)
+                if (older > result['data_de_fabricacao']):
+                    older = result['data_de_fabricacao']
+            except StopIteration:
+                break
+        return older
+
+    def data_de_validade(today, path, validate):
+        item = iter(path)
+        while item:
+            try:
+                result = next(item)
+                element = result['data_de_validade']
+                if (validate > element) and today < element:
+                    validate = result['data_de_validade']
+            except StopIteration:
+                break
+        return validate
