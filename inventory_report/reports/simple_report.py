d19e0e53b9983ffa1376d43b32817ecc43522c8e
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2021-12-13 23:24:31 -0300

    req 1 done

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,10 +1,20 @@
 import pandas as pd
+from datetime import date
 
-a = pd.read_csv("./inventory_report/data/inventory.csv")
-print(a)
 
 class SimpleReport:
 
     @staticmethod
     def generate(products):
-        return "abc"
+        df = pd.DataFrame(products)
+        current_date = str(date.today())
+        biggest_fac_date = df['data_de_fabricacao'].min()
+        vali_date = df['data_de_validade']
+        neareast_vali_date = vali_date[(vali_date > current_date)].min()
+        company_names = df['nome_da_empresa'].value_counts().idxmax()
+
+        toReturn = f"""Data de fabricação mais antiga: {biggest_fac_date}
+Data de validade mais próxima: {neareast_vali_date}
+Empresa com maior quantidade de produtos estocados: {company_names}
+"""
+        return toReturn
