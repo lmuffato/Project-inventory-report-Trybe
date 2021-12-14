1b73d8dffb3d10adb8f5bad9d7c0745d53862ceb
Lucas André <lucas.cancado89@gmail.com>
2021-12-14 13:40:39 -0300

    req05: xml importer

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory():
@@ -18,4 +19,7 @@ class Inventory():
             return cls.generate(report_type, dic_list)
         elif path.endswith('.json'):
             dic_list = JsonImporter.import_data(path)
+            return cls.generate(report_type, dic_list)
+        else:
+            dic_list = XmlImporter.import_data(path)
             return cls.generate(report_type, dic_list)
