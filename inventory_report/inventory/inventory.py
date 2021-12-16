ee5bee9b1bb1dc00b851ec2ec09920dff63cfa15
Felipe Flores <felipefloresvendas@gmail.com>
2021-12-16 08:23:34 -0300

    feat: requirement 3

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,18 @@
-# https://medium.com/data-hackers/escrita-e-leitura-de-arquivos-csv-em-python-6a256c608818
+# https://www.hashtagtreinamentos.com/como-trabalhar-com-arquivos-csv-no-python?gclid=Cj0KCQiA5OuNBhCRARIsACgaiqXIP6Kj1pXLiLT5BSkDQKmPxlAndhWV_Uksba5hWdw3uhdazARVqQYaAimrEALw_wcB
+# https://www.pythonforbeginners.com/code-snippets-source-code/python-os-listdir-and-endswith
+# https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html
 import csv
 from inventory_report.reports.simple_report import SimpleReport
-# from inventory_report.reports.simple_report import CompleteReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     def import_data(path, type):
-        if (type == "simples"):
-            with open(path) as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=',')
-
-                csv_reader.__next__()
-
-                SimpleReport.generate
-
-                # for row in csv_reader:
-                #     print(row[0] + ', ' + row[1] + ', ' + row[2])
+        with open(path, mode="r") as csv_file:
+            if path.endswith(".csv"):
+                reader = csv.DictReader(csv_file)
+                data = [row for row in reader]
+                if type == "simples":
+                    return SimpleReport.generate(data)
+                if type == "completo":
+                    return CompleteReport.generate(data)
