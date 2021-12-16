a21417e3f0f3114f652fc592089ee910eb77ec8f
AndreBarroso <andre_barroso17@hotmail.com>
2021-12-16 16:05:05 -0300

    feat: criada classe JsonImporter

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,14 @@
+import json
+
+from inventory_report.importer.importer import Importer
+
+
+class JsonImporter(Importer):
+    def import_data(path):
+        indexOfDot = path.find(".")
+        firstLetterOfExtention = path(indexOfDot + 1)
+        if firstLetterOfExtention(".j"):
+            with open(path, mode="r") as file:
+                data = json.load(file)
+                return data
+        raise ValueError()
