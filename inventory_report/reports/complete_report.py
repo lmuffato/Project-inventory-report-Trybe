e0b75780117eb609d00778dc9eff1ade82964fa4
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-17 15:57:31 -0300

    fix: corrige herança e corrige formataçao do relatorio

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,19 +1,17 @@
 from inventory_report.reports.simple_report import SimpleReport
 
 
-class CompleteReport():
-    def __init__(cls, simple_report=SimpleReport()):
-        cls.simple_report = simple_report
-
-    def create_report(cls, simple_report, products_stock):
-        report = '\n\nProdutos estocados por empresa:\n'
+class CompleteReport(SimpleReport):
+    def create_report(simple_report, products_stock):
+        report = '\nProdutos estocados por empresa: \n'
         for product in products_stock:
-            report += f'- {product}: {products_stock[product]} \n'
+            report += f'- {product}: {products_stock[product]}\n'
         return simple_report + report
 
-    def generate(cls, list):
-        simple_report = cls.simple_report.generate(list)
-        stock_by_company = cls.simple_report.verify_companies_stock(list)
-        complete_report = cls.create_report(
+    @classmethod
+    def generate(self, list):
+        simple_report = SimpleReport.generate(list)
+        stock_by_company = SimpleReport.verify_companies_stock(list)
+        complete_report = self.create_report(
             simple_report, stock_by_company)
         return complete_report
