4d81a0bccdae624d24e376a7006d928c63a76e81
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-17 14:41:36 -0300

    feat(complete_report): cria um relatorio simples com a classe simple report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -4,3 +4,6 @@ from inventory_report.reports.simple_report import SimpleReport
 class CompleteReport():
     def __init__(cls, simple_report=SimpleReport()):
         cls.simple_report = simple_report
+
+    def generate(cls, list):
+        simple_report = cls.simple_report.generate(list)
