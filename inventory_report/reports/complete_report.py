98e82fc699df1554e227641be293d5fda58ce32f
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-17 14:46:32 -0300

    feat(complete_report): cria o relatorio completo concatenando os dados do relatorio simples aos dados faltantes

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -5,7 +5,15 @@ class CompleteReport():
     def __init__(cls, simple_report=SimpleReport()):
         cls.simple_report = simple_report
 
+    def create_report(cls, simple_report, products_stock):
+        report = '\n\nProdutos estocados por empresa:\n'
+        for product in products_stock:
+            report += f'- {product}: {products_stock[product]} \n'
+        return simple_report + report
+
     def generate(cls, list):
         simple_report = cls.simple_report.generate(list)
         stock_by_company = cls.simple_report.verify_companies_stock(list)
-        
+        complete_report = cls.create_report(
+            simple_report, stock_by_company)
+        return complete_report
