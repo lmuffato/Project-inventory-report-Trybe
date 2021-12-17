20e8b3ca0b23900fa95744b663a0bafecf3ab34e
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-17 14:24:52 -0300

    feat(simple_report.py): adiciona funçao que gera data de validade mais proxima

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 
 class SimpleReport():
 
@@ -10,6 +12,20 @@ class SimpleReport():
                 earliest_manufact_date = product['data_de_fabricacao']
         return earliest_manufact_date
 
+    def closest_expiration_date(products):
+        closest_expiration_date = ''
+        today = date.today()
+        for product in products:
+            validate_date = product['data_de_validade']
+            if(today.strftime("%Y-%m-%D") > validate_date):
+                continue
+            elif closest_expiration_date == '':
+                closest_expiration_date = validate_date
+            elif closest_expiration_date > validate_date:
+                closest_expiration_date = validate_date
+        return closest_expiration_date
+
     @classmethod
     def generate(cls, list):
         earliest_manufact_date = cls.earliest_manufact_date(list)
+        closest_expiration_date = cls.closest_expiration_date(list)
