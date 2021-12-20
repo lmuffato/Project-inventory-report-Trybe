0f61ba9ad19189c83a72aa96d83d906818cef319
Mateus <mtauws@gmail.com>
2021-12-20 13:51:53 -0300

    chore: add inventory and importer

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,27 @@
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+import pandas as pd
+
+
+class Inventory():
+
+    @staticmethod
+    def import_data(path, report_type):
+        splited_path = path.split('.')
+        exten = splited_path[len(splited_path) - 1]
+        if exten == "csv":
+            df = pd.read_csv(path)
+
+        if exten == "xml":
+            df = pd.read_xml(path)
+            print(df)
+
+        if exten == "json":
+            df = pd.read_json(path)
+
+        if report_type == "simples":
+            report = SimpleReport.generate(df)
+            return report
+
+        report = CompleteReport.generate(df)
+        return report
