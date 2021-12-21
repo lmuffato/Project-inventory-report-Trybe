f72dc8eb5ebe52852d7a21c941ae7f9e04870f14
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-21 10:30:01 -0300

    feat(xml_importer): adiciona funcao que le arquivo xml e transforma em lista

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,29 @@
+# Source:
+# https://stackoverflow.com/questions/38869381/python-convert-xml-to-list
+import xml.etree.ElementTree as etree
+
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    def import_data(path):
+        indexOfDot = path.find(".") + 1
+        firstLetterOfExtention = path[indexOfDot]
+        if firstLetterOfExtention == "x":
+            lista = []
+            tree = etree.parse(path)
+            root = tree.getroot()
+            for child in root.findall('record'):
+                child_data = {}
+                for data in child:
+                    child_data[data.tag] = data.text
+                lista.append(child_data)
+            print(lista)
+            return lista
+        else:
+            raise ValueError('Arquivo inválido')
+
+
+# if(__name__ == '__main__'):
+#     data = XmlImporter()
+#     data.import_data('inventory_report/data/inventory.xml')
