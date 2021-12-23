db032b5fd9b82d6404a63dbb8f4e0472f4423460
Michael <michaelpetterson06.com.br>
2021-12-22 21:44:42 -0300

    req 06

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,10 @@
+from inventory_report.inventory.inventory import FormatTypes
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if not path.endswith('.xml'):
+            raise ValueError('Arquivo inválido')
+        return FormatTypes.type_xml(path)
