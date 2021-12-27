ae24b9d75a7bdda00ae98cee7ee570f138dd9a47
marcelofcdantas <marcelofcd@gmail.com>
2021-12-27 19:47:17 -0300

    fix: lint

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,10 +6,10 @@ class CompleteReport(SimpleReport):
 
     def generate(path):
         first_report = SimpleReport.generate(path)
-        counted_products = Counter(product["nome_da_empresa"] for product in path)
+        counted_items = Counter(product["nome_da_empresa"] for product in path)
         second_report = ""
-        for company in counted_products:
-            second_report += f"- {company}: {counted_products[company]}\n"
+        for company in counted_items:
+            second_report += f"- {company}: {counted_items[company]}\n"
         return(
             f"{first_report}\n"
             f"\n Produtos estocados por empresa:\n"
