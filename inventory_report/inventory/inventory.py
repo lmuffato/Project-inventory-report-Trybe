15a20dee17fdeda7e510caf2655aa391e0291d7b
Orlando Flores <orlando_flores@id.uff.br>
2021-12-27 17:58:04 -0300

    feat: requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -10,6 +11,16 @@ class Inventory:
 
         if path.endswith(".json"):
             file = pd.read_json(path).to_dict(orient="records")
+
+        if path.endswith(".xml"):
+            with open(path, mode="r") as file:
+                tree = ET.parse(file)
+                root = tree.getroot()
+                print(root)
+                data = [
+                    {elem.tag: elem.text for elem in child} for child in root
+                ]
+            return data
         return file
 
     @classmethod
