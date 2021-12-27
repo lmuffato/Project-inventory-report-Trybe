059edc8c238a71fa7b992d8d05cdce79f0d5fa4b
marcelofcdantas <marcelofcd@gmail.com>
2021-12-27 19:07:49 -0300

    feat: requisito 2 feito

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,7 +1,17 @@
 from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
 
 
 class CompleteReport(SimpleReport):
 
-    def generate():
-        return []
+    def generate(path):
+        first_report = SimpleReport.generate(path)
+        counted_products = Counter(product["nome_da_empresa"] for product in path)
+        second_report = ""
+        for company in counted_products:
+            second_report += f"- {company}: {counted_products[company]}\n"
+        return(
+            f"{first_report}\n"
+            f"\n Produtos estocados por empresa:\n"
+            f"- {second_report}"
+        )
