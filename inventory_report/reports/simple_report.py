4f8ce81a399a6664b71f5b63d6ee47fbb62568b7
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-29 14:59:00 -0300

    inicio da implementaçao do req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,3 +1,13 @@
 class SimpleReport:
-    def generate(self, data_dict):
-        return data_dict
+    @classmethod
+    def generate(cls, products):
+        
+        r1 = "Data de fabricação mais antiga: "
+        r2 = "Data de validade mais próxima: "
+        r3 = "Empresa com maior quantidade de produtos estocados: "
+
+        first_row = "{}{}".format(r1, '2019-09-13')
+        second_row = "{}{}".format(r2, '2023-01-17')
+        third_row = "{}{}".format(r3, 'sanofi-aventis U.S. LLC')
+
+        return "{}\n{}\n{}\n".format(first_row, second_row, third_row)
