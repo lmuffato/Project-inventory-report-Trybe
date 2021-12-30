e5de7a93bb2e4b4833ba6349810ced8531345a1e
Marcelo Maurício <mmauricio.design@gmail.com>
2021-12-30 09:51:08 -0300

    Feat 3: created a class Inventory with a function where read a CSV and return a data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,5 +3,23 @@ import csv
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
+
 class Inventory:
-  
+    __report_type = {
+      "simples": SimpleReport.generate,
+      "completo": CompleteReport.generate,
+    }
+
+    data_content = []
+
+    def import_data(path, type):
+        if path.endswith(".csv"):
+            with open(path) as csvfile:
+                file = csv.DictReader(csvfile)
+                data_content = [row for row in file]
+
+        return Inventory.__report_type[type](data_content)
+
+
+# Checagem com a terminação do arquivo
+# https://www.programiz.com/python-programming/methods/string/endswith
