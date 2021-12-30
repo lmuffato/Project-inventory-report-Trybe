67768cca8a5c8768a2c545426a1de0cf6599efce
Marcelo Maurício <mmauricio.design@gmail.com>
2021-12-30 10:16:06 -0300

    Feat 3: created a class Inventory with a function where read a XML file and return a data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -23,8 +24,20 @@ class Inventory:
             with open(path) as jsonfile:
                 data_content = json.load(jsonfile)
 
+        elif path.endswith(".xml"):
+            tree = ET.parse(path)
+            root = tree.getroot()
+            data_content = [
+                {cont.tag: cont.text for cont in record}
+                for record in root.findall("record")
+            ]
+
         return Inventory.__report_type[type](data_content)
 
 
-# Checagem com a terminação do arquivo
+# Checagem com a terminação do arquivo:
 # https://www.programiz.com/python-programming/methods/string/endswith
+
+# Leitura de arquivos XML e interação:
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+# https://www.geeksforgeeks.org/reading-and-writing-xml-files-in-python/
