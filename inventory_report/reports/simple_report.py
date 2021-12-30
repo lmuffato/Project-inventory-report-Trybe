202f56b8f6d0539bb0120b0fa825221b8e248746
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-30 11:10:32 -0300

    req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,12 +1,23 @@
+from inventory_report.reports.products_list import ProductsList
+
+
 class SimpleReport:
     @classmethod
     def generate(cls, products):
+        oldest_manufacture_date = ProductsList(
+            products
+        ).get_oldest_manufacture_date()
+        latest_expired_date = ProductsList(products).get_latest_expired_date()
+        company_with_largest_stock = ProductsList(
+            products
+        ).get_company_with_largest_stock()
+
         r1 = "Data de fabricação mais antiga: "
         r2 = "Data de validade mais próxima: "
         r3 = "Empresa com maior quantidade de produtos estocados: "
 
-        first_row = "{}{}".format(r1, '2019-09-13')
-        second_row = "{}{}".format(r2, '2023-01-17')
-        third_row = "{}{}".format(r3, 'sanofi-aventis U.S. LLC')
+        first_row = "{}{}".format(r1, oldest_manufacture_date)
+        second_row = "{}{}".format(r2, latest_expired_date)
+        third_row = "{}{}".format(r3, company_with_largest_stock)
 
         return "{}\n{}\n{}\n".format(first_row, second_row, third_row)
