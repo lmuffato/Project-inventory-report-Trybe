4d7aebdd9d44e83741627ecd88ef0b7c885f3d60
Gabriel <g.pereira.a.m@gmail.com>
2022-01-03 17:16:09 -0300

    feat(Req 3) - :sparkle: Requisito 3 feito

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,15 @@
+import csv
+
+class CsvImporter():
+    def import_data(path):
+        data = []
+
+        if path.endswith('.csv'):
+            with open(path, mode='r') as file:
+                lists = csv.DictReader(file)
+
+                for d in lists:
+                    data.append(d)
+
+            return data
+        raise ValueError('Arquivo inválido')
---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,18 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+from inventory_report.importer.csv_importer import CsvImporter
+
+class Inventory():
+    def leitura(path):
+        if path.endswith('.csv'):
+            return CsvImporter.import_data(path)
+
+    def import_data(path, type):
+        list = Inventory.leitura(path)
+        print(list)
+        if type == 'simples':
+            return SimpleReport.generate(list)
+        
+        if type == 'completo':
+            return CompleteReport.generate(list)
