3045c623c6bf82046325857c8792ed83fbac9f6b
rafael <rafittu@outlook.com>
2022-01-03 18:24:47 -0300

    feat req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,20 @@
-# req 1
+from datetime import datetime
+
+
+class SimpleReport:
+    def generate(data):
+        dt = datetime.now().strftime('%Y/%M/%D')
+
+        manufacturing_date = [item['data_de_fabricacao'] for item in data]
+
+        expiration_date = [item['data_de_validade'] for item in data
+                           if item['data_de_validade'] > dt]
+
+        company_largest_stock = [item['nome_da_empresa'] for item in data]
+
+        return (
+            f'Data de fabricação mais antiga: {min(manufacturing_date)}\n'
+            f'Data de validade mais próxima: {min(expiration_date)}\n'
+            f'Empresa com maior quantidade de produtos estocados: '
+            f'{max(company_largest_stock)}\n'
+        )
