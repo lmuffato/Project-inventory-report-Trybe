59b11ab5c93b0228680b4ae583e254d7b7d8a325
luanlsr <luanfswd@gmail.com>
2022-01-02 22:55:46 -0300

    refact simple_report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,30 +1,20 @@
-from collections import Counter
-from datetime import datetime
+import pandas as pd
+from datetime import date
 
 
 class SimpleReport:
+
     @staticmethod
-    def generate(inventory):
-        date_now = datetime.now().strftime("%Y%M%D")
-        all_companies = [item["nome_da_empresa"] for item in inventory]
-        oldest_manufactured_date = min(
-            [item["data_de_fabricacao"] for item in inventory]
-        )
-        closest_expiration_date = min(
-            [
-                item["data_de_validade"]
-                for item in inventory
-                if item["data_de_validade"] > date_now
-            ]
-        )
+    def generate(products):
+        df = pd.DataFrame(products)
+        data_atual = str(date.today())
+        oldest_manufactured_date = df['data_de_fabricacao'].min()
+        validate = df['data_de_validade']
+        closest_expiration_date = validate[(validate > data_atual)].min()
+        companie_more_stock = df['nome_da_empresa'].value_counts().idxmax()
 
-        companie_with_more_stock, _quantity = Counter(
-            all_companies
-        ).most_common(1)[0]
-
-        return (
-            f"Data de fabricação mais antiga: {oldest_manufactured_date}\n"
-            f"Data de validade mais próxima: {closest_expiration_date}\n"
-            f"Empresa com maior quantidade de produtos estocados: "
-            f"{companie_with_more_stock}\n"
-        )
+        return (f"Data de fabricação mais antiga: {oldest_manufactured_date}\n"
+                f"Data de validade mais próxima: {closest_expiration_date}\n"
+                f"Empresa com maior quantidade de produtos estocados: "
+                f"{companie_more_stock}\n"
+                )
