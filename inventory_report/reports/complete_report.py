89474f6fcfe1a62769984868d5f363fa480e6c83
Bruno Mendes <mendessbruno@gmail.com>
2022-01-04 11:34:28 -0300

    resolvido desafio 2

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,29 +1,29 @@
-from _typeshed import Self
-from simple_report import simple_report
+from inventory_report.reports.simple_report import SimpleReport
 
 
-class completeReport:
-    def get_all_companies(list):
-        company_list = set()
-        for data in list:
-            company_list.add(data["nome_da_empresa"])
+class CompleteReport(SimpleReport):
+    def complement_report(company_dict):
+        complementary_report = '\nProdutos estocados por empresa: \n'
+        for product in company_dict:
+            complementary_report += f'- {product}: {company_dict[product]}\n'
+        return (complementary_report)
 
-        return company_list
-
-    def get_quantity_of_products_by_company(list):
-        company_list = Self.get_all_companies(list)
+    def count_products(list):
+        company_list = []
         company_dict = {}
-
+        for data in list:
+            company_list.append(data["nome_da_empresa"])
         for company in company_list:
             sum = 0
             for data in list:
                 if (data["nome_da_empresa"] == company):
                     sum += 1
             company_dict.update({company: sum})
-        return company_dict
+        return (company_dict)
 
-    def generate():
-        simple_report.generate(list)
-        company_dict = Self.get_quantity_of_products_by_company(list)
-        print("produstos estocados por empresas:")
-        print(company_dict)
+    @classmethod
+    def generate(self, list):
+        company_dict = self.count_products(list)
+        report = SimpleReport.generate(list)
+        complementary_report = self.complement_report(company_dict)
+        return (report + complementary_report)
