877ee20d4ea6faddafce5576bddae91272ab9a72
Bruno Mendes <mendessbruno@gmail.com>
2022-01-04 16:44:13 -0300

    resolvido desafio 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import json
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -9,9 +10,22 @@ class Inventory:
             list = json.load(file)
         return list
 
+    def read_XML(path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        list = []
+        for item in root.findall('record'):
+            item_dict = {}
+            for data in item:
+                item_dict.update({data.tag: data.text})
+            list.append(item_dict)
+        return list
+
     def check_type(self, path):
         if ".json" in path:
             return self.read_JSON(path)
+        if ".xml" in path:
+            return self.read_XML(path)
         else:
             breakpoint
 
