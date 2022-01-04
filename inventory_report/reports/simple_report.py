c2cfda8cbf7fa29c9dc7ace03fdb0b4d72cc28c5
Rafael Geronimo <git_config_user_email>
2022-01-04 16:41:55 -0300

    feat: req1.3 | validate company with largest amount products in stock

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
+
 class SimpleReport:
-  def generate(data):
-    today = datetime.now().strftime('%Y-%M-%D')
-    manufacture_date = [item['data_de_fabricacao'] for item in data]
-    expiration_date = [item['data_de_validade'] for item in data
-      if item['data_de_validade'] > today]
-    return (
-      f'Data de fabricação mais antiga: {min(manufacture_date)}',
-      f'Data de validade mais próxima: {min(expiration_date)}',
-      f'Empresa com maior quantidade de produtos estocados:'
-    )
+    def generate(data):
+        today = datetime.now().strftime('%Y-%M-%D')
+        manufacture_date = [item['data_de_fabricacao'] for item in data]
+        expiration_date = [item['data_de_validade'] for item in data
+                           if item['data_de_validade'] > today]
+        company_stock = [item['nome_da_empresa'] for item in data]
+        return (
+          f'Data de fabricação mais antiga: {min(manufacture_date)}\n'
+          f'Data de validade mais próxima: {min(expiration_date)}\n'
+          f'Empresa com maior quantidade de produtos estocados: '
+          f'{max(company_stock)}\n'
+        )
