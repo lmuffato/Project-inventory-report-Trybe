9163068bb11868b95f1d875a21dce20de56a0d10
Felipe Flores <felipefloresvendas@gmail.com>
2022-01-04 18:04:22 -0300

    fix: evaluator

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,5 +1,6 @@
-#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary
-#https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
+# https://stackoverflow.com/questions/2148119
+# /how-to-convert-an-xml-string-to-a-dictionary
+# https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
 
 from inventory_report.importer.importer import Importer
 import xmltodict
