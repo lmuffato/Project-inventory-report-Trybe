41998c63bc8755b56f49fac8deb5d59b16eff3d7
mallmann02 <leomallmann020302@gmail.com>
2022-01-04 17:06:03 -0300

    feat: csv importer

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,13 @@
+import csv
+from .importer import Importer
+
+
+class CsvImporter(Importer):
+    def import_data(path):
+        if path.endswith('.csv'):
+            with open(path, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                data = [line for line in csv_reader]
+                return data
+        else:
+            raise ValueError('Invalid file extension')
