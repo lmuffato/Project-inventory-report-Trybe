4e2d59907adaa9bd4a9e1e11b5db96f257db1cbc
Rmgreco <rafaelmgreco@hotmail.com>
2022-01-04 14:10:23 -0300

    req3 dnv

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,10 +8,11 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    def import_data(path, type):
+    @classmethod
+    def import_data(cls, path, type):
         if path.endswith(".csv"):
             with open(path, mode="r") as csv_file:
-                data = list(csv.DictReader(csv_file))
+                data = [list(csv.DictReader(csv_file))]
         if type == "simples":
             return SimpleReport.generate(data)
         if type == "completo":
