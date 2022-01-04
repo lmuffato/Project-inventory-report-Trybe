c764d692b8a3585765a1f9ba53ef4e71a4276226
Micaela Alves <micaela.alves93@gmail.com>
2022-01-04 20:32:24 -0300

    fix - requisito 3 importar_um_arquivo_csv_completo

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,27 +2,23 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 # https://docs.python.org/pt-br/3/tutorial/datastructures.html
-class CompleteReport():
+class CompleteReport:
     def get_products_by_company(stock):
-        company_name = []
+        company_name = [product["nome_da_empresa"] for product in stock]
         list_without_duplicates = []
-        for product in stock:
-            company_name.append(product["nome_da_empresa"])
-            list_without_duplicates.append(product["nome_da_empresa"])
         result = "Produtos estocados por empresa: \n"
+
+        for prod in company_name:
+            if prod not in list_without_duplicates:
+                list_without_duplicates.append(prod)
+
         for company in list_without_duplicates:
-            if company_name.count(company) > 1:
-                list_without_duplicates.remove(company)
-            result += (
-                f"- {company}: "
-                f"{company_name.count(company)}\n"
-            )
+            result += f"- {company}: " f"{company_name.count(company)}\n"
+
         return result
 
     @classmethod
     def generate(cls, stock):
         simple_report = SimpleReport.generate(stock)
-        result = (
-            cls.get_products_by_company(stock)
-        )
+        result = cls.get_products_by_company(stock)
         return simple_report + "\n" + result
