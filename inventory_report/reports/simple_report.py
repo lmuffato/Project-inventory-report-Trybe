0390f775ec5b8597a0fdc2e5bc812a17d8a7fbd7
Rafael Geronimo <git_config_user_email>
2022-01-04 16:06:30 -0300

    feat: method variables renamed in english

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,11 +3,11 @@ from datetime import datetime
 class SimpleReport:
   def generate(data):
     today = datetime.now().strftime('%Y-%M-%D')
-    data_de_fabricacao = [item['data_de_fabricacao'] for item in data]
-    data_de_validade = [item['data_de_validade'] for item in data
-                        if item['data_de_validade'] > today]
+    manufacture_date = [item['data_de_fabricacao'] for item in data]
+    expiration_date = [item['data_de_validade'] for item in data
+      if item['data_de_validade'] > today]
     return (
-      f'Data de fabricação mais antiga: {min(data_de_fabricacao)}',
-      f'Data de validade mais próxima: {min(data_de_validade)}',
+      f'Data de fabricação mais antiga: {min(manufacture_date)}',
+      f'Data de validade mais próxima: {min(expiration_date)}',
       f'Empresa com maior quantidade de produtos estocados:'
     )
