c1cf7acd3a0d618d50d5fcd5de7b05fa22ff4915
mallmann02 <leomallmann020302@gmail.com>
2022-01-04 15:28:16 -0300

    feat: import_data_json (req4)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -9,6 +10,13 @@ class Inventory:
             with open(path, 'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 data = [line for line in csv_reader]
+                if report_type == 'simples':
+                    return SimpleReport.generate(data)
+                else:
+                    return CompleteReport.generate(data)
+        elif path.endswith('.json'):
+            with open(path, 'r') as json_file:
+                data = json.load(json_file)
                 if report_type == 'simples':
                     return SimpleReport.generate(data)
                 else:
