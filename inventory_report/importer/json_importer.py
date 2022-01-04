5a05371245114f6788c6d6a7ab88af9ed0b365df
mallmann02 <leomallmann020302@gmail.com>
2022-01-04 17:07:49 -0300

    feat: json importer

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,12 @@
+import json
+from .importer import Importer
+
+
+class JsonImporter(Importer):
+    def import_data(path):
+        if path.endswith('.json'):
+            with open(path, 'r') as json_file:
+                data = json.load(json_file)
+                return data
+        else:
+            raise ValueError('Invalid file extension')
