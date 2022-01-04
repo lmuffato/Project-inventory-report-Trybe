e775da33033f2f2f0015e0847c100d4948c4fa87
Ana Ventura <anaventura1811@gmail.com>
2022-01-04 15:55:37 -0300

    feat: pequena refatoração na classe StockUtils para reaproveitar mesmo método

--- inventory_report/reports/stock_utils.py
+++ inventory_report/reports/stock_utils.py
@@ -6,15 +6,18 @@ class StockUtils:
 
     def get_absolute_date(date_obj): return abs(date_obj - date.today())
 
+    def get_date(stock, str_key):
+        return [date.fromisoformat(item[str_key]) for item in stock]
+
     def get_oldest_manufacture_date(stock, str_key):
         return max(
-          [date.fromisoformat(product[str_key]) for product in stock],
+          StockUtils.get_date(stock, str_key),
           key=StockUtils.get_absolute_date
         )
 
     def get_closest_due_date(stock, str_key):
         return min(
-          [date.fromisoformat(product[str_key]) for product in stock],
+          StockUtils.get_date(stock, str_key),
           key=StockUtils.get_absolute_date
         )
 
