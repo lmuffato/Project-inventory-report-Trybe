980b4060c8bbc5bfb94b9135c0b08289a8b9d06f
Micaela Alves <micaela.alves93@gmail.com>
2022-01-04 20:38:55 -0300

    feat - importação json /xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,7 @@
 import os
 from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -13,6 +15,8 @@ class Inventory:
         }
         file_extension = {
             ".csv": CsvImporter.import_data,
+            ".json": JsonImporter.import_data,
+            ".xml": XmlImporter.import_data,
         }
         data = file_extension[extensao](path)
         result = report_types[type_of_report](data)
