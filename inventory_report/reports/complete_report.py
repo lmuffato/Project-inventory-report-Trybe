7fa13e6f8ad2caf0296577fad8d383501411ef0a
Ana Ventura <anaventura1811@gmail.com>
2022-01-04 16:38:27 -0300

    feat: implementa req. 2, class CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,15 +2,33 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
+    @classmethod
     def generate(self, stock):
-        self.generate_complete_report(stock)
+        return self.generate_complete_report(stock)
 
     def generate_complete_report(stock):
-        company_name_list = []
-        key = ''
-        stock_count = 0
+        key = 0
+        total_stock_count = SimpleReport.get_stock_count(
+          stock, 'nome_da_empresa'
+        )
+
+        total_stock_keys = list(total_stock_count.keys())
+        total_stock_values = list(total_stock_count.values())
+
+        total_count = len(total_stock_values)
+
+        detailed_report = []
+
+        while key < total_count:
+            detailed_report.append(
+              f'- {total_stock_keys[key]}: {total_stock_values[key]}\n'
+            )
+            key += 1
+
+        complete_report_message = '\nProdutos estocados por empresa: \n'
 
         simple_report = SimpleReport.generate(stock)
-        result = f'- {company_name_list[key]}: {stock_count}\n'
 
-# - Nome da empresa: qty
+        return simple_report + complete_report_message + "".join(
+          detailed_report
+        )
