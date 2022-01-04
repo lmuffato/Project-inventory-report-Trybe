37057881f0cf9e2e975fee1a81af0aa77e10c01f
Gui-lira <gui.lira.martins@gmail.com>
2022-01-03 23:49:38 -0300

    4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,29 @@
 import csv
+import json
 from inventory_report.reports import simple_report, complete_report
 
 
 class Inventory():
-    def import_data(file, type):
-        typeDict = {
+    typeDict = {
             "simples": simple_report.SimpleReport.generate,
             "completo": complete_report.CompleteReport.generate,
         }
+
+    def csvReader(file, type):
         dictList = []
         with open(file, newline='') as csvFile:
             reader = csv.DictReader(csvFile)
             for row in reader:
                 dictList.append(row)
-        return typeDict[type](dictList)
+        return Inventory.typeDict[type](dictList)
+
+    def jsonReader(file, type):
+        with open(file, 'r') as jsonFile:
+            data = json.load(jsonFile)
+            return Inventory.typeDict[type](data)
+
+    def import_data(file, type):
+        if file.endswith("csv"):
+            return Inventory.csvReader(file, type)
+        elif file.endswith("json"):
+            return Inventory.jsonReader(file, type)
