be4b774e0a99f94dddaf7826b565014fa99e27e2
Felipe Flores <felipefloresvendas@gmail.com>
2022-01-04 18:00:41 -0300

    feat: to requirement 6

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,4 +1,8 @@
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary
+#https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
+
 from inventory_report.importer.importer import Importer
+import xmltodict
 
 
 class XmlImporter(Importer):
@@ -6,4 +10,6 @@ class XmlImporter(Importer):
     def import_data(path):
         if not path.endswith(".xml"):
             raise ValueError("Arquivo inválido")
-            pass
+        with open(path) as xml_file:
+            data = xmltodict.parse(xml_file.read())["dataset"]["record"]
+            return [dict(item) for item in data]
