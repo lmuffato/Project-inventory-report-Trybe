227a7b1ae8e7ce204bed6620d75e5e7d7f12012c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-04 16:06:45 -0300

    Feat: Implementa class 'CompleteReport' e seus metodos 'serialize', 'generate'

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,6 +2,21 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
+    def serialize(list):
+        count_product_manufacturing = ""
+        for item in list:
+            count_product_manufacturing += f"- {item}: {list[item]}\n"
+        return count_product_manufacturing
 
-    def generate():
-        return []
+    @classmethod
+    def generate(self, path):
+
+        products_stocked_report = self.serialize(
+            SimpleReport.count_products(path)
+        )
+
+        return (
+            f"{SimpleReport.generate(path)}\n"
+            f"Produtos estocados por empresa: \n"
+            f"{products_stocked_report}"
+        )
