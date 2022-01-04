8e202727d583704fb48b2a9e209fd4d4a6e52846
MarcioFujita <hanker78@gmail.com>
2022-01-04 14:43:17 -0300

    fix: Requisitos 3-4-5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,31 +7,30 @@ import xmltodict
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type):
+    def verify_extension(cls, path):
         if path.endswith('.csv'):
             with open(path) as file:
                 data_reader = csv.DictReader(file)
                 data = [item for item in data_reader]
-            if (type == 'simples'):
-                return SimpleReport.generate(data)
-            elif (type == 'completo'):
-                return CompleteReport.generate(data)
+            return data
         elif path.endswith('.json'):
             with open(path) as file:
                 data_reader = json.load(file)
                 data = [item for item in data_reader]
-            if (type == 'simples'):
-                return SimpleReport.generate(data)
-            elif (type == 'completo'):
-                return CompleteReport.generate(data)
+            return data
         elif path.endswith('.xml'):
             with open(path) as file:
                 data_reader = xmltodict.parse(file.read())
                 data = [item for item in data_reader['dataset']['record']]
-            if (type == 'simples'):
-                return SimpleReport.generate(data)
-            elif (type == 'completo'):
-                return CompleteReport.generate(data)
+            return data
+
+    @classmethod
+    def import_data(cls, path, type):
+        extension_data = cls.verify_extension(path)
+        if (type == 'simples'):
+            return SimpleReport.generate(extension_data)
+        elif (type == 'completo'):
+            return CompleteReport.generate(extension_data)
 
 #  Fonte para xmltodict:https://python-guide-pt-br.readthedocs.io/
 # pt_BR/latest/scenarios/xml.html
