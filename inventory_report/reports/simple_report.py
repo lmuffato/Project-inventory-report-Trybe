9c4d3986973fac86b65e3e6fe4a7271bb633af66
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-04 16:04:59 -0300

    Feat: Implementa class 'SimpleReport' e seus metodos 'oldest_manufacturing_date', 'nearest_expiration_date', 'count_products', 'generate'

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,30 +1,56 @@
 from datetime import datetime
 from collections import Counter
-from inventory_report.reports.functions import Functions
 
 
 class SimpleReport:
-
-    def generate(path):
-        today = datetime.today().strftime('%Y-%m-%d')
-        older = "3000-01-01"
-        validate = "3000-01-01"
-        nome = ''
-        nomes = []
-        older = Functions.data_de_fabricacao(today, path, older)
-        validate = Functions.data_de_validade(today, path, validate)
+    def oldest_manufacturing_date(today, path, older):
         item = iter(path)
         while item:
             try:
                 result = next(item)
-                nomes.append(result['nome_da_empresa'])
+                if older > result["data_de_fabricacao"]:
+                    older = result["data_de_fabricacao"]
             except StopIteration:
                 break
-        nomes = Counter(nomes)
-        nome = nomes.most_common()
-        nome = nome[0][0]
-        return(
-            f"Data de fabricação mais antiga: {older}\n"
-            f"Data de validade mais próxima: {validate}\n"
+        return older
+
+    def nearest_expiration_date(today, path, validate):
+        item = iter(path)
+        while item:
+            try:
+                result = next(item)
+                element = result["data_de_validade"]
+                if (validate > element) and today < element:
+                    validate = result["data_de_validade"]
+            except StopIteration:
+                break
+        return validate
+
+    def count_products(list):
+        lista = []
+        item = iter(list)
+        while item:
+            try:
+                result = next(item)
+                lista.append(result["nome_da_empresa"])
+            except StopIteration:
+                break
+        __quantidades = Counter(lista)
+        return __quantidades
+
+    @classmethod
+    def generate(self, path):
+        today = datetime.today().strftime("%Y-%m-%d")
+        date_reference = "3000-01-01"
+        date_older = self.oldest_manufacturing_date(
+            today, path, date_reference
+        )
+        nearest_validity = self.nearest_expiration_date(
+            today, path, date_reference
+        )
+        nome = self.count_products(path).most_common()[0][0]
+        return (
+            f"Data de fabricação mais antiga: {date_older}\n"
+            f"Data de validade mais próxima: {nearest_validity}\n"
             f"Empresa com maior quantidade de produtos estocados: {nome}\n"
         )
