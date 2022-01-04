5c0a2c167445a453ffac082d27c25f13407cf10c
Rafael Geronimo <git_config_user_email>
2022-01-04 16:02:15 -0300

    feat: req1.2 | validate closest expiration date

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 class SimpleReport:
   def generate(data):
+    today = datetime.now().strftime('%Y-%M-%D')
     data_de_fabricacao = [item['data_de_fabricacao'] for item in data]
-    data_de_validade = 
+    data_de_validade = [item['data_de_validade'] for item in data
+                        if item['data_de_validade'] > today]
     return (
       f'Data de fabricação mais antiga: {min(data_de_fabricacao)}',
-      f'Data de validade mais próxima:',
+      f'Data de validade mais próxima: {min(data_de_validade)}',
       f'Empresa com maior quantidade de produtos estocados:'
     )
