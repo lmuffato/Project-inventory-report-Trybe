359f6d696b32acb86a54f3154b219d223a33d96c
Renan Costa <renancosta21@gmail.com>
2022-01-04 15:56:09 -0300

    refactoring and updating Inventory class + req4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,21 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
 
 
 class Inventory:
     @classmethod
     def import_data(cls, path, type):
-        csv_content = cls.read(path)
+        file_content = cls.read(path)
         if type == "simples":
-            return SimpleReport.generate(csv_content)
+            return SimpleReport.generate(file_content)
         elif type == "completo":
-            return CompleteReport.generate(csv_content)
+            return CompleteReport.generate(file_content)
 
     @classmethod
     def read(cls, path):
         if path.endswith(".csv"):
             return CsvImporter.import_data(path)
+        elif path.endswith(".json"):
+            return JsonImporter.import_data(path)
