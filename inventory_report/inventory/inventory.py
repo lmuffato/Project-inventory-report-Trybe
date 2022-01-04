53e9fce62f8d452966fc08543de5cb42ee1fd2ee
mallmann02 <leomallmann020302@gmail.com>
2022-01-04 16:08:16 -0300

    feat: import_data_xml (req4)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,7 +1,15 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
+
+def send_report(report_type, data):
+    if report_type == 'simples':
+        return SimpleReport.generate(data)
+    else:
+        return CompleteReport.generate(data)
 
 
 class Inventory:
@@ -10,14 +18,23 @@ class Inventory:
             with open(path, 'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 data = [line for line in csv_reader]
-                if report_type == 'simples':
-                    return SimpleReport.generate(data)
-                else:
-                    return CompleteReport.generate(data)
+                return send_report(report_type, data)
+
         elif path.endswith('.json'):
             with open(path, 'r') as json_file:
                 data = json.load(json_file)
-                if report_type == 'simples':
-                    return SimpleReport.generate(data)
-                else:
-                    return CompleteReport.generate(data)
+                return send_report(report_type, data)
+        else:
+            tree = ET.parse(path)
+            dataset = tree.getroot()
+            data = [
+                {
+                    el.tag: el.text
+                    for el in record
+                }
+                for record in dataset
+            ]
+            if report_type == 'simples':
+                return SimpleReport.generate(data)
+            else:
+                return CompleteReport.generate(data)
