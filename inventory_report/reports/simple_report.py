d8e77b6504d2f8f68774518ddff785ac0cc4fe9a
MaxRudim <max.hermsdorff@gmail.com>
2022-01-04 21:46:43 -0300

    Requisito 01

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,24 @@
-#
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+    @staticmethod
+    def generate(stock):
+        stored_fabrication_date = []
+        stored_validation_date = []
+        today_date = datetime.today().strftime('%Y-%m-%d')
+        enterprise_name = []
+        for product in stock:
+            stored_fabrication_date.append(product['data_de_fabricacao'])
+            if product['data_de_validade'] > today_date:
+                stored_validation_date.append(product['data_de_validade'])
+            enterprise_name.append(product['nome_da_empresa'])
+        enterprise_count = Counter(enterprise_name)
+        formated_string = (
+            f"Data de fabricação mais antiga: {min(stored_fabrication_date)}\n"
+            f"Data de validade mais próxima: {min(stored_validation_date)}\n"
+            f"Empresa com maior quantidade de produtos "
+            f"estocados: {max(enterprise_count)}\n"
+        )
+        return formated_string
