e4a504e41b6e57a45a7b051f0ea54596ff0f98df
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-05 13:54:27 -0300

    fix: requirement 1 - greatest company

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,22 +1,25 @@
 from inventory_report.reports.check_date import CheckDate
+from statistics import mode
 
 
 class SimpleReport:
     @classmethod
     def generate(cls, data):
-        fabrication_dates = cls.__get_fabrication_dates(data)
+        fabrication_dates = [row["data_de_fabricacao"] for row in data]
         oldest_fabrication = CheckDate.filter_oldest(fabrication_dates)
 
-        expiration_dates = cls.__get_expiration_dates(data)
+        expiration_dates = [row["data_de_validade"] for row in data]
         closer_expiration = CheckDate.filter_closest(expiration_dates)
 
-        # greatest_company = ''
+        companies_list = [row["nome_da_empresa"] for row in data]
+        greatest_company = cls.__filter_company(companies_list)
+
         return (f"Data de fabricação mais antiga: {oldest_fabrication}\n"
                 f"Data de validade mais próxima: {closer_expiration}\n"
-                f"Empresa com maior quantidade de produtos estocados: ")
+                f"Empresa com maior quantidade de produtos estocados: "
+                f"{greatest_company}\n")
 
-    def __get_fabrication_dates(data):
-        return [row["data_de_fabricacao"] for row in data]
-
-    def __get_expiration_dates(data):
-        return [row["data_de_validade"] for row in data]
+    def __filter_company(data):
+        most_frequent = mode(data)
+        print(most_frequent)
+        return most_frequent
