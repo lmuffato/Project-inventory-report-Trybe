0e710d8f1dfa78a477d3834bcd5dc7ee58850b3e
luanlsr <luanfswd@gmail.com>
2022-01-06 08:16:36 -0300

    req6 xml finalizado

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,1 +1,14 @@
+import xml.etree.ElementTree as ET
+from inventory_report.importer.importer import Importer
 
+
+class XmlImporter(Importer):
+    def import_data(name):
+        if name.endswith(".xml"):
+            tree = ET.parse(name)
+            root = tree.getroot()
+            return [
+                {el.tag: el.text for el in record}
+                for record in root.findall("record")
+            ]
+        raise ValueError("Arquivo inválido")
