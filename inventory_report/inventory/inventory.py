de08e74d37613db22519ce1fa10d6cc4d1571405
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-06 12:16:09 -0300

    feat: mudança para avaliaçao individual

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,6 @@
-from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.xml_importer import XmlImporter
 
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
