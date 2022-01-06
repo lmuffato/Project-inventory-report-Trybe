da617b7a4bdbe4e9e7cedbeac66460a906ee7438
Rafael Geronimo <git_config_user_email>
2022-01-06 09:42:40 -0300

    feat: req6 | abstract class `import_data` with three inherited classes

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    def import_data(path):
+        with open(path) as file:
+            if 'xml' in path:
+                reading = ET.parse(file).getroot()
+                data = [
+                    {elem.tag: elem.text for elem in child}
+                    for child in reading
+                ]
+
+                return data
+            else:
+                raise ValueError('Arquivo inválido')
