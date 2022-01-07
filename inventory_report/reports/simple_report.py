ed99485808b7cf65901669c5b53404e546b3dd14
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-07 19:26:47 -0300

    delete dict test

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,37 +2,6 @@ import datetime
 import collections
 
 
-dict = [
-  {
-    "id": 1,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "Nature",
-    "data_de_fabricacao": "2020-07-04",
-    "data_de_validade": "2023-02-09",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-  {
-    "id": 2,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2018-07-05",
-    "data_de_validade": "2022-02-09",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-  {
-    "id": 2,
-    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "1988-07-05",
-    "data_de_validade": "2021-01-09",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  }
-]
-
-
 class SimpleReport:
     def generate(file_dict):
         agora = datetime.date.today()
@@ -57,6 +26,3 @@ class SimpleReport:
         b = f'Data de validade mais próxima: {validade_min_date}\n'
         c = f'Empresa com maior quantidade de produtos estocados: {cpny}\n'
         return(a + b + c)
-
-
-# print(SimpleReport.generate(dict))
