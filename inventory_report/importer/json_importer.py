e78601b89349e2e992b77559f7b53a61284a98ba
Nathalia Zebral <natzebral@gmail.com>
2022-01-06 22:18:48 -0300

    requisito pasta importer

---
+++ inventory_report/importer/json_importer.py
@@ -0,0 +1,11 @@
+from inventory_report.importer.importer import Importer
+import json
+
+
+class JsonImporter(Importer):
+    def import_data(file_path):
+        if file_path.endswith(".json"):
+            with open(file_path, "r") as jsonfile:
+                return json.load(jsonfile)
+
+        raise ValueError("Arquivo inválido")
---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,14 @@
+from inventory_report.importer.importer import Importer
+import xml.etree.ElementTree as ET
+
+
+class XmlImporter(Importer):
+    def import_data(file_path):
+        if file_path.endswith(".xml"):
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            return [
+                {el.tag: el.text for el in record}
+                for record in root.findall("record")
+            ]
+        raise ValueError("Arquivo inválido")
