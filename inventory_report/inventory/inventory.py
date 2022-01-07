0c891a504b91cd9230c1c09e525c51ebfcc0a206
Nathalia Zebral <natzebral@gmail.com>
2022-01-06 22:10:18 -0300

    requisitos arquivo inventory

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,38 @@
+import csv
+import xml.etree.ElementTree as ET
+import json
+
+
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    __report_functions = {
+        "simples": SimpleReport.generate,
+        "completo": CompleteReport.generate,
+    }
+
+    def import_data(file_path, report_type):
+        array_data = []
+
+        if file_path.endswith(".csv"):
+            with open(file_path) as csvfile:
+                reader = csv.DictReader(csvfile)
+                array_data = [row for row in reader]
+
+        elif file_path.endswith(".json"):
+            with open(file_path, "r") as file:
+                array_data = json.load(file)
+
+        elif file_path.endswith(".xml"):
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            array_data = [
+                {el.tag: el.text for el in record}
+                for record in root.findall("record")
+            ]
+
+        else:
+            raise ValueError("Arquivo inválido")
+        return Inventory.__report_functions[report_type](array_data)
