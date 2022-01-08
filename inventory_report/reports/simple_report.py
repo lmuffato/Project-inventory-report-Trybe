5c4f0ee49958e97f6980d31c11d2566a6a9eef57
Eduardo Seije <eduardoseije@gmail.com>
2022-01-08 15:54:31 -0300

    Fix - req 01 - correção na data de fabricação

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,13 +5,13 @@ from datetime import datetime
 class SimpleReport:
     def generate(data):
         curr_date = datetime.now().strftime('%Y-%M-%D')
-        manufacture_date = [item['date_de_fabricacao'] for item in data]
+        manufacture_date = [item['data_de_fabricacao'] for item in data]
         expiration_date = [item['data_de_validade'] for item in data
                            if item['data_de_validade'] > curr_date]
         company_inventory = [item['nome_da_empresa'] for item in data]
         return (
-          f'Data de fabricação mais antiga: {min(manufacture_date)}'
-          f'Data de validade mais próxima: {min(expiration_date)}'
+          f'Data de fabricação mais antiga: {min(manufacture_date)}\n'
+          f'Data de validade mais próxima: {min(expiration_date)}\n'
           f'Empresa com maior quantidade de produtos estocados: '
           f'{max(company_inventory)}\n'
         )
