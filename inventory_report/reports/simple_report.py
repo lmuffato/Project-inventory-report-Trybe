95a933d0fe4b1fb4932b9a122d7610b5a240beb7
Eduardo Seije <eduardoseije@gmail.com>
2022-01-08 15:36:59 -0300

    Feat req 1 - criação do método generate - simple_report.py

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,20 @@
 # Simple_report
+from datetime import datetime
+
+
+class SimpleReport:
+    def generate(data):
+        curr_date = datetime.now().strftime('%Y-%M-%D')
+        manufacture_date = [item['date_de_fabricacao'] for item in data]
+        expiration_date = [item['data_de_validade'] for item in data
+                           if item['data_de_validade'] > curr_date]
+        company_inventory = [item['nome_da_empresa'] for item in data]
+        return (
+          f'Data de fabricação mais antiga: {min(manufacture_date)}'
+          f'Data de validade mais próxima: {min(expiration_date)}'
+          f'Empresa com maior quantidade de produtos estocados: '
+          f'{max(company_inventory)}\n'
+        )
+
+# Referência - utilizamos o repositório de Igson
+# e Rafael como base para desenvolvimento.
