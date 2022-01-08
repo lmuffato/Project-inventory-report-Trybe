c1c48468ac2a69e9a875e53e995ee0ee9853b3f0
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-08 15:07:45 -0300

    requrirement 01: simpleReport

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+
+class SimpleReport:
+    def generate(data):
+        m_date = []
+
+        expiration_date = []
+        date_now = '2022-01-01'
+        comp = []
+
+        for element in data:
+            if element["data_de_fabricacao"]:
+                m_date.append(element["data_de_fabricacao"])
+            if element["data_de_validade"] > date_now:
+                expiration_date.append(element["data_de_validade"])
+            if element["nome_da_empresa"]:
+                comp.append(element["nome_da_empresa"])
+
+        counter = Counter(comp)
+        result = max(counter)
+
+        messages = [
+            f"Data de fabricação mais antiga: {min(m_date)}\n",
+            f"Data de validade mais próxima: {min(expiration_date)}\n",
+            f"Empresa com maior quantidade de produtos estocados: {result}\n",
+        ]
+
+        return "".join(messages)
