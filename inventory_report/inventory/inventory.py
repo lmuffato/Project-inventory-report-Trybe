a5bdcad69d9a15a9cc995e54a972fecd1f556f6b
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-01-08 16:44:49 -0300

    fix flake8

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,22 +8,22 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     def import_data(path, type):
         file_extension = path.split(".")[1]
-        if file_extension == 'csv':
+        if file_extension == "csv":
             with open(path) as file:
                 csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
                 header, *data = csv_file
                 data = [header, *data]
-        if file_extension == 'json':
+        if file_extension == "json":
             with open(path) as file:
                 data = json.load(file)
-        if file_extension == 'xml':
+        if file_extension == "xml":
             tree = ET.parse(path)
             root = tree.getroot()
             data = [
                 {el.tag: el.text for el in record}
                 for record in root.findall("record")
             ]
-        if type == 'simples':
+        if type == "simples":
             return SimpleReport.generate(data)
-        if type == 'completo':
-            return CompleteReport.generate(data)
+
+        return CompleteReport.generate(data)
