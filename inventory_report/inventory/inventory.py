4349765759afde97d496ac042479b3a73871358b
MaxRudim <max.hermsdorff@gmail.com>
2022-01-08 15:51:44 -0300

    Requisito 04

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,14 +1,20 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     def import_data(path, type):
-        with open(path) as file:
-            csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
-            header, *data = csv_file
-            data = [header, *data]
+        file_extension = path.split(".")[1]
+        if file_extension == 'csv':
+            with open(path) as file:
+                csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
+                header, *data = csv_file
+                data = [header, *data]
+        if file_extension == 'json':
+            with open(path) as file:
+                data = json.load(file)
         if type == 'simples':
             return SimpleReport.generate(data)
         if type == 'completo':
