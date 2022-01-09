56794b5c4d3843670590d7d8de0fb47a4d2d571b
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2022-01-08 21:23:55 -0300

    feat(req7): finish it

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -3,13 +3,18 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
+from inventory_report.importer.csv_importer import CsvImporter
+
 
 class InventoryRefactor(InventoryIterator):
     importer = None
+    max_index = None
+    data = []
+    index = 0
     type = ''
 
     def __init__(self, importer):
-        self.importer = importer()
+        self.importer = importer
 
     def import_data_validation(self, path, type):
         if(type not in ['simples', 'completo']):
@@ -21,15 +26,40 @@ class InventoryRefactor(InventoryIterator):
 
     def import_data(self, path, type):
         self.import_data_validation(path, type)
+        out = None
 
         if(type == 'simples'):
             products = self.importer.import_data(path)
-            return SimpleReport.generate(products)
+            self.data = [*self.data, *products]
+            out = SimpleReport.generate(products)
         elif (type == 'completo'):
             products = self.importer.import_data(path)
-            return CompleteReport.generate(products)
+            self.data = [*self.data, *products]
+            out = CompleteReport.generate(products)
         else:
             raise 'Não deveria ter chegado aqui'
 
+        self.max_index = len(self.data) - 1
+
+        return out
+
     def __iter__(self):
         return super().__iter__()
+
+    def __next__(self):
+        if(self.max_index > self.index):
+            out = self.data[self.index]
+            self.index += 1
+            return out
+        else:
+            raise StopIteration
+
+
+inventory = InventoryRefactor(CsvImporter)
+
+inventory.import_data("inventory_report/data/inventory.csv", "simples")
+
+iterator = iter(inventory)
+first_item = next(iterator)
+
+print(iterator, first_item)
