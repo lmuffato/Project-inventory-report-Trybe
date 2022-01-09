8be2cf8433191b8bbba1f5b1c4dfe30a047a6af0
lucas-paz <lucas.hpaz@outlook.com>
2022-01-09 20:46:51 -0300

    creates InventoryIterator class

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,19 @@
+# from collections.abc import Iterator
+# from typing import Any
+
+
+class InventoryIterator:
+    def __init__(self, collection):
+        self._collection = collection
+        self._position = 0
+
+    def __next__(self):
+        try:
+            value = self._collection[self._position]
+            self._position += 1
+        except IndexError:
+            raise StopIteration()
+
+            return value
+
+
