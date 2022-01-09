d8051cf3766dacb1b1a64a13d142a68797ba820e
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-01-09 11:15:16 -0300

    Fix: indentaçao

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -7,7 +7,6 @@ from inventory_report.importer.importer import Importer
 class XmlImporter(Importer):
     def import_data(path):
         extension_type = path.split(".")[1]
-        # try:
         if (extension_type == "xml"):
             with open(path) as file_reports:
                 string_from_xml = xmltodict.parse(file_reports.read())
@@ -15,7 +14,6 @@ class XmlImporter(Importer):
                 dict_from_string = json.dumps(string_from_xml)
                 reports = json.loads(dict_from_string)["dataset"]["record"]
                 return reports
-        # except ValueError:
         else:
             raise ValueError("Arquivo inválido")
 
