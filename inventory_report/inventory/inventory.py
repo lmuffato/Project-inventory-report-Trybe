f49667851314380347e8c95281f265c2be09e472
vinicios <soicinivt@gmail.com>
2022-01-10 01:27:20 -0300

    refactor: class Inventory in inventory.py

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import csv
 import json
+import pathlib
+
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -9,21 +11,19 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory(CompleteReport, SimpleReport):
     @classmethod
     def import_data(cls, path, report_type):
-        extension_type = path.split(".")[1]
+        extension_type = pathlib.Path(path).suffix
 
-        if (extension_type == "csv"):
+        if extension_type == ".csv":
             return cls.import_data_csv(path, report_type)
-        elif (extension_type == "json"):
+        elif extension_type == ".json":
             return cls.import_data_json(path, report_type)
-        elif (extension_type == "xml"):
+        elif extension_type == ".xml":
             return cls.import_data_xml(path, report_type)
 
     def import_data_csv(path, report_type):
         with open(path, mode="r") as file_reports:
             dict_from_csv = csv.DictReader(file_reports)
-            reports = list(
-                report for report in dict_from_csv
-            )
+            reports = list(report for report in dict_from_csv)
             if report_type == "simples":
                 return SimpleReport.generate(reports)
             elif report_type == "completo":
@@ -32,9 +32,7 @@ class Inventory(CompleteReport, SimpleReport):
     def import_data_json(path, report_type):
         with open(path, mode="r") as file_reports:
             dict_from_json = json.load(file_reports)
-            reports = list(
-                report for report in dict_from_json
-            )
+            reports = list(report for report in dict_from_json)
             if report_type == "simples":
                 return SimpleReport.generate(reports)
             elif report_type == "completo":
