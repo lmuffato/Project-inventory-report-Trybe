f3722d0fa543cf88712a197d3555b918eaa585a3
Carlos Campo <carlosgabrielcampo@gmail.com>
2022-01-09 22:21:22 -0300

    starting requirement 7

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,9 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+class InventoryRefactor():
+    def import_data(cls, path, type):
+        
