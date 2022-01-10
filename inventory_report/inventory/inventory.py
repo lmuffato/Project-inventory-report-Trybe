61e2867cb6b09abfa8170a205367cb9c4ad00c48
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-01-10 15:09:24 -0300

    feat: req04 and 05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,40 @@
 import csv
+import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def import_data(path, type):
+    def import_data(path, _type):
         with open(path, 'r') as file:
-            if path.endswith('.csv'):
-                reader = csv.DictReader(file)
-                data = [row for row in reader]
-            if type == 'simples':
-                return SimpleReport.generate(data)
-            if type == 'completo':
-                return CompleteReport.generate(data)
+            reader = csv.DictReader(file)
+            data = [row for row in reader]
+            return data
+
+    def import_json(path, _type):
+        with open(path, 'r') as file:
+            return json.load(file)
+
+    def import_xml(path, _type):
+        with open(path, 'r') as file:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            data = [
+                {el.tag: el.text for el in record}
+                for record in root.findall('record')
+            ]
+            return data
+
+    @classmethod
+    def import_data(cls, path, type):
+        list = []
+        if path.endswith('.csv'):
+            list = cls.import_csv(path, type)
+        if path.endswith('.json'):
+            list = cls.import_json(path, type)
+        if path.endswith('.xml'):
+            list = cls.import_xml(path, type)
+        if type == 'completo':
+            return CompleteReport.generate(list)
+        return SimpleReport.generate(list)
