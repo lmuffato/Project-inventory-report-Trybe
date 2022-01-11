ec71e58eb0a1a98f69af7bd7b58862aebf3cad93
bob-mw <wend3llsouza@gmail.com>
2022-01-10 21:51:36 -0300

    feat(req 6): abstract class

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,11 +1,19 @@
-import xmltodict
-from .importer import Importer
+
+
+import xml.etree.ElementTree as ET
+from inventory_report.importer.importer import Importer
 
 
 class XmlImporter(Importer):
     def import_data(path):
         if '.xml' in path:
-            with open(path) as file:
-                file_data = xmltodict.parse(file.read())
-                return file_data['dataset']['record']
+            result = []
+            tree = ET.parse(path)
+            root = tree.getroot()
+            for record in root.findall('record'):
+                lines = {}
+                for product in record:
+                   lines[product.tag] = product.text
+                result.append(lines)
+            return result
         raise ValueError('Arquivo inválido')
