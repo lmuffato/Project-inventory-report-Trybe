db57aba36e4aa5716e75ddac19558c8993b4490c
Ana Ventura <anaventura1811@gmail.com>
2022-01-11 11:59:23 -0300

    fix: remove código morto

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -6,8 +6,6 @@ class XmlImporter(Importer):
     def import_data(path):
         with open(path, mode="r") as xml_file:
             if path.endswith(".xml"):
-                # reader = ET.parse(xml_file)
-                # root = reader.getroot()
                 root = ET.fromstringlist(xml_file, parser=None)
                 data = [
                   {it.tag: it.text for it in el} for el in root
