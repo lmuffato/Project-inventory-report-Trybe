721139f48b4b9feda50f641a4077a135ef7cbbe5
lucas-paz <lucas.hpaz@outlook.com>
2022-01-11 14:54:37 -0300

    fix replacement at data attribute

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -6,15 +6,10 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 class InventoryRefactor:
     def __init__(self, importer):
         self.importer = importer
-        self.data = {}
-
-    __report_functions = {
-        "simples": SimpleReport.generate,
-        "completo": CompleteReport.generate,
-    }
+        self.data = []
 
     def import_data(self, file_name, type):
-        self.data = self.importer.import_data(file_name)
+        self.data.extend(self.importer.import_data(file_name))
         return self.data
 
     def __iter__(self):
