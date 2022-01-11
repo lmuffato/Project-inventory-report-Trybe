ec2ab5be80f067f2743b5004d8002c066d1fce3f
bob-mw <wend3llsouza@gmail.com>
2022-01-10 22:06:21 -0300

    feat(req 6):add xmltodict in requiriments

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -3,9 +3,10 @@ from .importer import Importer
 
 
 class XmlImporter(Importer):
+    @staticmethod
     def import_data(path):
         if '.xml' in path:
-            with open(path) as file:
+            with open(path, encoding='utf-8') as file:
                 file_data = xmltodict.parse(file.read())
                 return file_data['dataset']['record']
         raise ValueError('Arquivo inválido')
