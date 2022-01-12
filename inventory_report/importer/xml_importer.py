1855610e0b6ebf963ecea0c44a85c278e2a6399e
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-12 11:21:00 -0300

    fix: req6

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as E
+import xmltodict
 from .importer import Importer
 
 
@@ -7,6 +7,6 @@ class XmlImporter(Importer):
     def import_data(path):
         if '.xml' in path:
             with open(path, encoding='utf-8') as file:
-                file_data = E.parse(file.read())
+                file_data = xmltodict.parse(file.read())
                 return file_data['dataset']['record']
         raise ValueError('Arquivo inválido')
