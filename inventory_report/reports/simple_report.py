0cf6866a589fb0f3c9f9e62ab4547de0cc9254ce
Stanrley Barbosa <3011stan@gmail.com>
2022-01-15 18:02:00 -0300

    Req 1 done:test

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,17 @@
 # evaluator
+from datetime import datetime
+
+
+class SimpleReport:
+    def generate(products):
+        current_date = datetime.now().strftime(' %Y/%M/%D')
+        manufacturing_date = [product['data_de_fabricacao']
+                              for product in products]
+        expiration_date = [product['data_de_validade'] for product in products
+                           if product['data_de_validade'] > current_date]
+        stock = [product['nome_da_empresa'] for product in products]
+        return (
+          f'Data de fabricação mais antiga: {max(manufacturing_date)}'
+          f'Data de validade mais próxima: {min(expiration_date)}'
+          f'Empresa com maior quantidade de produtos estocados: {max(stock)}'
+        )
