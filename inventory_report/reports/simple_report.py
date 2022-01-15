e6602b38785cb7b0f4440acc6522308a49054acd
ScriptCamilo <script.camilo@gmail.com>
2022-01-15 20:09:51 -0300

    refactor: Refactor SimpleReport generate method

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,5 +1,5 @@
-# evaluator
 from datetime import datetime
+from collections import Counter
 
 
 class SimpleReport:
@@ -10,8 +10,18 @@ class SimpleReport:
         expiration_date = [product['data_de_validade'] for product in products
                            if product['data_de_validade'] > current_date]
         stock = [product['nome_da_empresa'] for product in products]
+        companies = Counter(stock)
+
+        quantity = ''
+        counter = 0
+
+        for company, value in companies.items():
+            if (counter < value):
+                quantity = company
+                counter = value
+
         return (
           f'Data de fabricação mais antiga: {min(manufacturing_date)}\n'
           f'Data de validade mais próxima: {min(expiration_date)}\n'
-          f'Empresa com maior quantidade de produtos estocados: {max(stock)}\n'
+          f'Empresa com maior quantidade de produtos estocados: {quantity}\n'
         )
