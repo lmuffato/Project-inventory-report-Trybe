2ae273abc4a3e3aeac1934e09bbd7db0753f58f1
ScriptCamilo <script.camilo@gmail.com>
2022-01-15 23:57:56 -0300

    feat: Add req5 done

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -21,13 +22,21 @@ class Inventory:
                 products.append(product)
         return products
 
-    # def get_products_from_xml(path):
-    #     products = []
-    #     with open(path) as file:
-    #         products_csv = csv.DictReader(file)
-    #         for product in products_csv:
-    #             products.append(product)
-    #     return products
+    def get_products_from_xml(path):
+        products = []
+        with open(path) as file:
+            products_xml = ET.parse(file).getroot()
+            for product in products_xml:
+                products.append({
+                                    product[0].tag: product[0].text,
+                                    product[1].tag: product[1].text,
+                                    product[2].tag: product[2].text,
+                                    product[3].tag: product[3].text,
+                                    product[4].tag: product[4].text,
+                                    product[5].tag: product[5].text,
+                                    product[6].tag: product[6].text,
+                                })
+        return products
 
     @classmethod
     def import_data(cls, path, type):
@@ -38,6 +47,8 @@ class Inventory:
             products = cls.get_products_from_csv(path)
         elif (fileType == 'json'):
             products = cls.get_products_from_json(path)
+        else:
+            products = cls.get_products_from_xml(path)
 
         if (type == 'simples'):
             return SimpleReport.generate(products)
