077e65eccf29a4cf993bb5d0326ebedd1f5191b4
lmuffato <lmiremuffato@gmail.com>
2022-01-19 02:05:09 -0300

    requisito 8/home/lucas/Documentos/projects/Trybe/sd-010-a-inventory-report/.venv/bin/python /home/lucas/Documentos/projects/Trybe/sd-010-a-inventory-report/inventory_report/main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,35 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    if(len(sys.argv) != 3):  # os argumentos passados pelo terminal devem ser 3
+        return print('Verifique os argumentos', file=sys.stderr)
+    print(sys.stderr)
+    relatory_type = sys.argv[2]
+    file_path = sys.argv[1]
+
+    if (sys.argv[1].endswith('.csv')):
+        report = InventoryRefactor(CsvImporter)
+        sys.stdout.write(report.import_data(file_path, relatory_type))
+
+    elif (sys.argv[1].endswith('json')):
+        report = InventoryRefactor(JsonImporter)
+        sys.stdout.write(report.import_data(file_path, relatory_type))
+
+    elif (sys.argv[1].endswith('xml')):
+        report = InventoryRefactor(XmlImporter)
+        sys.stdout.write(report.import_data(file_path, relatory_type))
+
+# Teste manual
+
+# Executar no terminal
+# Teste 01
+inventory_report 'inventory_report/data/inventory.csv' 'simples'
+# Teste 02
+# inventory_report 'inventory_report/data/inventory.json' 'simples'
+# Teste 03
+# inventory_report 'inventory_report/data/inventory.xml' 'completo'
