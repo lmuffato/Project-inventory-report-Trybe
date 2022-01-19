530d20de18c9cac1d78c670848066475b84a49e9
AdelinoJnr <adelinojunior@gmail.com>
2022-01-19 18:17:29 -0300

    Fix: Requeriment 3

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,18 @@
 import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    def import_data(file):
-        if ".csv" in file:
-            with open(file) as data:
-                return list(csv.DictReader(data))
-        raise ValueError("Arquivo inválido")
+    def send_report(report_type, data):
+        if report_type == 'simples':
+            return SimpleReport.generate(data)
+        else:
+            return CompleteReport.generate(data)
+
+    def import_data(cls, path, report_type):
+        if path.endswith('.csv'):
+            with open(path) as data:
+                csv_reader = csv.DictReader(data)
+                data = [line for line in csv_reader]
+                return cls.send_report(report_type, data)
