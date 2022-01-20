de8666a54a2ac6a0a9d6b19515327da6c72a8066
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-20 09:53:43 -0300

    refact: Turning 3 scopes in 1 scope (test)

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -11,13 +11,11 @@ class SimpleReport:
             if oldest_date > gokuSsj["data_de_fabricacao"]:
                 oldest_date = gokuSsj["data_de_fabricacao"]
 
-        for gokuSsj2 in products:
             if vality_date > current_date:
-                vality_date.append(gokuSsj2["data_de_validade"])
+                vality_date.append(gokuSsj["data_de_validade"])
 
-        for gokuSsj3 in products:
-            if gokuSsj3["nome_da_empresa"]:
-                industries.append(gokuSsj3["nome_da_empresa"])
+            if gokuSsj["nome_da_empresa"]:
+                industries.append(gokuSsj["nome_da_empresa"])
 
         return (
             f"Data de fabricação mais antiga: {oldest_date}\n",
