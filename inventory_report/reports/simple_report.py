46fecef4b505bce611abfef6a6cc47e072e0ebd3
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-20 11:28:49 -0300

    fix: debug all errors (wrong sintax and  string)

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,23 +3,24 @@ from datetime import datetime
 
 class SimpleReport:
     def generate(products):
-        current_date = datetime.now().strftime("%Y-%M-%D")
+        current_date = datetime.today().strftime("%Y-%M-%D")
         oldest_date = products[0]["data_de_fabricacao"]
-        vality_date = products[0]["data_de_validade"]
+        vality_date = []
         industries = []
+
         for gokuSsj in products:
             if oldest_date > gokuSsj["data_de_fabricacao"]:
                 oldest_date = gokuSsj["data_de_fabricacao"]
 
-            if vality_date > current_date:
+            if gokuSsj["data_de_validade"] > current_date:
                 vality_date.append(gokuSsj["data_de_validade"])
 
             if gokuSsj["nome_da_empresa"]:
                 industries.append(gokuSsj["nome_da_empresa"])
 
         return (
-            f"Data de fabricação mais antiga: {oldest_date}\n",
-            f"Data de validade mais próxima: {vality_date}\n",
-            f"Empresa com maior quantidade"
-            f"de produtos estocados: {industries}\n",
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {min(vality_date)}\n"
+            f"Empresa com maior quantidade de produtos"
+            f" estocados: {max(industries)}\n"
         )
