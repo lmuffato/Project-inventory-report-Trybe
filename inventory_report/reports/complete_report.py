6e463c04a34303967c6139445dd516ee0e4d7465
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-20 22:14:24 -0300

    feat: req 2 finally done

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,16 @@
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def generate(products):
+        simple_report = SimpleReport.generate(products)
+        stock = [product["nome_da_empresa"] for product in products]
+        enterprises = Counter(stock)
+
+        string = "\nProdutos estocados por empresa: \n"
+
+        for gokuSsj2, vegetaSsj2 in enterprises.items():
+            string += f"- { gokuSsj2 }: { vegetaSsj2 }\n"
+
+        return simple_report + string
