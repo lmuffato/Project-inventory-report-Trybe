9521d1489af039d39bcbcea6f2db3f70092c593c
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-22 17:01:50 -0300

    feat/fix: importer file refc and integrations done

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,27 @@
-import csv
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
-    def import_data(path, type):
-        list = []
-        with open(path) as file:
-            products_csv = csv.DictReader(file)
-            for product in products_csv:
-                list.append(product)
+    def generate(type, list):
 
         if type == "simples":
             return SimpleReport.generate(list)
         if type == "completo":
             return CompleteReport.generate(list)
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        if path.endswith(".csv"):
+            dic_list = CsvImporter.import_data(path)
+            return cls.generate(report_type, dic_list)
+        if path.endswith(".json"):
+            dic_list = JsonImporter.import_data(path)
+            return cls.generate(report_type, dic_list)
+        if path.endswith(".xml"):
+            dic_list = XmlImporter.import_data(path)
+            return cls.generate(report_type, dic_list)
