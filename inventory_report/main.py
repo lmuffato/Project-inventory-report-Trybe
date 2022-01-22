f748f47d4284e46944f2dcad5f520146181c9b55
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2022-01-22 15:17:15 -0300

    Requisito 8.1 a 8.4 concluidos

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,19 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        return sys.stderr.write("Verifique os argumentos")
+    extension = sys.argv[1][-3:]
+    path = sys.argv[1]
+    report_type = sys.argv[2]
+
+    if "csv" in extension:
+        report = InventoryRefactor(CsvImporter)
+        print(report.import_data(path, report_type), end="")
+    else:
+        report = InventoryRefactor(JsonImporter)
+        print(report.import_data(path, report_type), end="")
