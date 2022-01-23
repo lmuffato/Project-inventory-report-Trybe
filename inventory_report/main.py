604d227d53551aafc20da7803eb3baa702e2174f
fomes <contato.felipeluna@gmail.com>
2022-01-23 09:33:52 -0300

    finish main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,27 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.json_importer import JsonImporter as jsonimp
+from inventory_report.importer.csv_importer import CsvImporter as csvimp
+from inventory_report.importer.xml_importer import XmlImporter as xmlimp
+
+
 def main():
+    if(len(sys.argv) != 3):
+        print('Verifique os argumentos', file=sys.stderr)
+
+    fileType = sys.argv[1].split('.')[-1]
+
+    if(fileType == 'json'):
+        report = InventoryRefactor(jsonimp)
+        final_report = report.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(final_report)
+    elif(fileType == 'csv'):
+        report = InventoryRefactor(csvimp)
+        final_report = report.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(final_report)
+    elif(fileType == 'xml'):
+        report = InventoryRefactor(xmlimp)
+        final_report = report.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(final_report)
+
     pass
