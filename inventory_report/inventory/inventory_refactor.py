87aae8d9c4d015beb2477031838fca92fba93b56
Andre Sampaio <andresampaiolm@gmail.com>
2022-01-24 05:41:50 -0300

    fix req 7 and complete_report

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -6,10 +6,9 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 
 class InventoryRefactor(Iterable):
-    data = []
-
     def __init__(self, Importer):
         self.importer = Importer
+        self.data = []
 
     def __iter__(self):
         return InventoryIterator(self.data)
