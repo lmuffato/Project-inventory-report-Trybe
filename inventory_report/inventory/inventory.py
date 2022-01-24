044af0697457f76925fe3d6abf244f32cff5581c
Southier <patrick.southier@hotmail.com>
2022-01-24 05:57:58 -0300

    feat: implementing the import_data and import_json methods at Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -10,10 +11,16 @@ class Inventory:
             result_data = [result_row for result_row in path_reader]
             return result_data
 
+    def import_json(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+
     @classmethod
     def import_data(cls, file_path, report_type):
         if file_path.endswith(".csv"):
             data_list = cls.import_csv(file_path)
+        elif file_path.endswith(".json"):
+            data_list = cls.import_json(file_path)
 
         if report_type == "completo":
             return CompleteReport.generate(data_list)
