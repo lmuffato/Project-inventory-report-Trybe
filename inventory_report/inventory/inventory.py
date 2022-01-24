e30020bd7e457dbfbb7dd53edab8b3af8c70665a
Southier <patrick.southier@hotmail.com>
2022-01-24 06:16:10 -0300

    feat: implementing the import_data and import_xml methods at Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -15,12 +16,24 @@ class Inventory:
         with open(file_path, 'r') as file:
             return json.load(file)
 
+    def import_xml(file_path):
+        with open(file_path, 'r') as file:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            result_data = [
+                {el.tag: el.text for el in record}
+                for record in root.findall('record')
+            ]
+            return result_data
+
     @classmethod
     def import_data(cls, file_path, report_type):
         if file_path.endswith(".csv"):
             data_list = cls.import_csv(file_path)
         elif file_path.endswith(".json"):
             data_list = cls.import_json(file_path)
+        elif file_path.endswith(".xml"):
+            data_list = cls.import_xml(file_path)
 
         if report_type == "completo":
             return CompleteReport.generate(data_list)
