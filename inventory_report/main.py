0fd47bf94c030f4ea918e0efd73f4f70d5a2000c
Andre Sampaio <andresampaiolm@gmail.com>
2022-01-24 05:48:42 -0300

    last push

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,8 +1,8 @@
 import sys
 from inventory_report.inventory.inventory_refactor import InventoryRefactor
 from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
 
 
 def main():
