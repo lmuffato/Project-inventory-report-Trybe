ea4b9f2ea450b89f6b1338e6d74693d543959861
Andre Sampaio <andresampaiolm@gmail.com>
2022-01-24 05:31:48 -0300

    fix lint issues

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,12 +1,13 @@
 from collections.abc import Iterable
 
-from inventory_report.importer.importer import Importer
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
+
 class InventoryRefactor(Iterable):
     data = []
+
     def __init__(self, Importer):
         self.importer = Importer
 
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -4,6 +4,7 @@ from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
 
+
 def main():
     if len(sys.argv) != 3:
         return sys.stderr.write("Verifique os argumentos\n")
